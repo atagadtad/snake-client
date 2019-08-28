815c9fc6f026d414a3edf8199d925b218cc0057f
atagadtad <aarontagadtad@gmail.com>
2019-08-28 16:49:48 +0000

    finished move

--- client.js
+++ client.js
@@ -15,15 +15,27 @@ const connect = function () {
   });
 
   // prints console after establishing connection with server
+  // using the .on notation and 'connect'
   conn.on('connect', () => {
     console.log('Successfully connected to game server')
-  });
 
-  conn.on('connect', () => {
     conn.write('Name: AJT');
+
+
+    // setTimeout(function () {
+    //   conn.write('Move: up')
+    // }, 1000);
+    // setTimeout(function () {
+    //   conn.write('Move: down')
+    // }, 3000);
+    // setTimeout(function () {
+    //   conn.write('Move: left')
+    // }, 2000);
+    // setTimeout(function () {
+    //   conn.write('Move: right')
+    // }, 5000);
+
   });
-
-
 
   return conn;
 }
