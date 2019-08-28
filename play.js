c9d28ffb1be1d85f8927e1b490fa0336e3b5816b
atagadtad <aarontagadtad@gmail.com>
2019-08-28 17:29:43 +0000

    refactored stdin. functions into new file called input.js

--- play.js
+++ play.js
@@ -1,23 +1,9 @@
 const { connect } = require('./client');
-const stdin = process.stdin;
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
+
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
 setupInput();
 
-// uses ctr + c to exit the game using stdin.
-const handleUserInput = function () {
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-};
-handleUserInput();
