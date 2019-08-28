5af478ae95f7faa80c8cd5e5a9fa324a068067ed
atagadtad <aarontagadtad@gmail.com>
2019-08-28 17:02:37 +0000

    added stdin

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require('./client');
+const stdin = process.stdin;
 console.log('Connecting ...');
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+setupInput();
+
+// uses ctr + c to exit the game using stdin.
+const handleUserInput = function () {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+handleUserInput();
